d1b0e91e8e23fe4737f0fc67c52aabd85f59c2bf
Donat <donatborkovskij@gmail.com>
2023-10-14 17:35:29 +0200

    commit

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month >= 1 && month <= 12) {
+            int daysInMonth;
+
+            if (month == 2) {
+                daysInMonth = 28;
+            } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                daysInMonth = 30;
+            } else {
+                daysInMonth = 31;
+            }
+
+            System.out.println(daysInMonth);
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
